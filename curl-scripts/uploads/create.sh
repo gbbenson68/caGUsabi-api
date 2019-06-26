f569597a5dd4b15b1400c46fc8b4c7fb6780d725
SashaBausheva <abausheva@gmail.com>
2019-06-26 15:22:43 -0400

    Modify curl-scripts for upload and update routes
    - Updated curl-scripts for create, destroy, and update actions
    - Start working on ownership in upload_routes.js

--- curl-scripts/uploads/create.sh
+++ curl-scripts/uploads/create.sh
@@ -2,15 +2,18 @@
 
 API="http://localhost:4741"
 URL_PATH="/uploads"
+TOKEN="1327f0f3e71a6961f9308b211749884d"
 
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
   --data '{
     "upload": {
       "name": "'"${NAME}"'",
-      "description": "'"${DESCRIPTION}"'"
+      "description": "'"${DESCRIPTION}"'",
+      "url": "'"${URL}"'"
     }
   }'
 
